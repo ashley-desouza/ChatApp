14495898096bd48126882c31881afaa66ec01d91
ashley-desouza <ashley.desouza@ischool.berkeley.edu>
2016-09-19 00:26:00 -0700

    Added ability for sockets to connect to private rooms.

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,18 @@ let socket = io();
 const username = getQueryVariable('username') || 'Anonymous';
 const room = getQueryVariable('room');
 
+socket.on('connect', () => {
+	console.log('Connected to socket.io server!');
+
+	socket.emit('joinRoom', {
+		username,
+		room
+	});
+});
+
 socket.on('message', data => {
 	const $serverMessages = jQuery('#serverMessages');
+	const $chatRoom = jQuery('.roomTitle');
 
 	// http://momentjs.com/docs/#/parsing/utc/
 	const momentTimeStamp = moment.utc(data.timestamp); // UNIX Timestamp as a Moment Object
@@ -15,6 +25,8 @@ socket.on('message', data => {
 	const localTimeStamp = momentTimeStamp.local(); // Local Timestamp based on browser TimeZone
 
 	const localTimeStampFormatted = localTimeStamp.format('hh:mm a'); // Display time as 01:10 AM
+
+	$chatRoom.text(room);
 
 	$serverMessages.append(`<p><strong>${localTimeStampFormatted} ${data.username}:</strong></p>`);
 	$serverMessages.append(`<p>${data.text}</p>`);
@@ -25,7 +37,7 @@ document.addEventListener('submit', event => {
 
 	let message = document.getElementById('messageBox').value;
 
-	socket.emit('message', { text: message, username });
+	socket.emit('message', { text: message, username, room });
 
 	document.getElementById('messageBox').value = '';
 });
--- server.js
+++ server.js
@@ -8,11 +8,24 @@ const io = require('socket.io')(http); // http://socket.io/docs/
 const moment = require('moment');
 
 const PORT = process.env.PORT || 3000;
+let clientInfo = {};
 
 app.use(express.static(__dirname + '/public/'));
 
 io.on('connection', (socket) => {
 	console.log('Connection Established through Socket.IO');
+
+	socket.on('joinRoom', user => {
+		socket.join(user.room);
+
+		clientInfo[socket.id] = user;
+
+		socket.broadcast.to(clientInfo[socket.id].room).emit('message', {
+			text: `${clientInfo[socket.id].username} has joined! Say Hi!`,
+			username: 'System',
+			timestamp: moment().valueOf()
+		});
+	});
 
 	socket.emit('message', {
 		text: 'Welcome',
@@ -25,8 +38,10 @@ io.on('connection', (socket) => {
 
 		// http://momentjs.com/docs/#/displaying/unix-timestamp-milliseconds/
 		data.timestamp = moment().valueOf(); // Get UNIX TimeStamp (milliseconds)
-		
-		io.emit('message', data); // Send to all connections including emitter
+
+		// io.emit('message', data); // Send to all connections including emitter
+
+		io.to(clientInfo[socket.id].room).emit('message', data); // Send to all connections including emitter
 	});
 });
 
