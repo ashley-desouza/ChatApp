48f74cc678f9669cd208f92ea71df29cb2b385a6
ashley-desouza <ashley.desouza@ischool.berkeley.edu>
2016-09-18 22:31:20 -0700

    Added Username and Time details. Also created a new page to join the chat room.

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,21 @@
 
 let socket = io();
 
+const username = getQueryVariable('username') || 'Anonymous';
+const room = getQueryVariable('room');
+
 socket.on('message', data => {
 	const $serverMessages = jQuery('#serverMessages');
 
+	// http://momentjs.com/docs/#/parsing/utc/
+	const momentTimeStamp = moment.utc(data.timestamp); // UNIX Timestamp as a Moment Object
+
+	// http://momentjs.com/docs/#/manipulating/local/
+	const localTimeStamp = momentTimeStamp.local(); // Local Timestamp based on browser TimeZone
+
+	const localTimeStampFormatted = localTimeStamp.format('hh:mm a'); // Display time as 01:10 AM
+
+	$serverMessages.append(`<p><strong>${localTimeStampFormatted} ${data.username}:</strong></p>`);
 	$serverMessages.append(`<p>${data.text}</p>`);
 });
 
@@ -13,7 +25,7 @@ document.addEventListener('submit', event => {
 
 	let message = document.getElementById('messageBox').value;
 
-	socket.emit('message', { text: message });
+	socket.emit('message', { text: message, username });
 
 	document.getElementById('messageBox').value = '';
 });
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http); // http://socket.io/docs/
+const moment = require('moment');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,10 +14,18 @@ app.use(express.static(__dirname + '/public/'));
 io.on('connection', (socket) => {
 	console.log('Connection Established through Socket.IO');
 
-	socket.emit('message', { text: 'Welcome' });
+	socket.emit('message', {
+		text: 'Welcome',
+		timestamp: moment().valueOf(),
+		username: 'System'
+	});
 
 	socket.on('message', data => {
 		// socket.broadcast.emit('message', data); -- Send to all connections EXCEPT emitter
+
+		// http://momentjs.com/docs/#/displaying/unix-timestamp-milliseconds/
+		data.timestamp = moment().valueOf(); // Get UNIX TimeStamp (milliseconds)
+		
 		io.emit('message', data); // Send to all connections including emitter
 	});
 });
