365a1bf5291e7ff2d4bd7d3c377abbd4ce56882a
ashley-desouza <ashley.desouza@ischool.berkeley.edu>
2016-09-25 19:56:41 -0700

    Added babel compiler

--- dist/server.js
+++ dist/server.js
@@ -1,35 +1,37 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http); // http://socket.io/docs/
-const moment = require('moment');
+var http = require('http').Server(app);
+var io = require('socket.io')(http); // http://socket.io/docs/
+var moment = require('moment');
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // An Object to contain each connection (having an unique socket id).
 // The key is the socket id and the value is another object that contains the username and room
 // of the socket.
-let clientInfo = {};
+var clientInfo = {};
 
 app.use(express.static(__dirname + './../public/'));
 
 // Function to generate System messages
-const sendSysMessage = message => ({
-	text: message,
-	username: 'System',
-	timestamp: moment().valueOf()
-});
+var sendSysMessage = function sendSysMessage(message) {
+	return {
+		text: message,
+		username: 'System',
+		timestamp: moment().valueOf()
+	};
+};
 
-const sendCurrentConnectedUsers = socket => {
-	const userInfo = clientInfo[socket.id];
-	let users = [];
+var sendCurrentConnectedUsers = function sendCurrentConnectedUsers(socket) {
+	var userInfo = clientInfo[socket.id];
+	var users = [];
 
 	// Check if the socket belongs to any chatrooms
 	if (typeof clientInfo[socket.id] !== 'undefined') {
-		users = Object.keys(clientInfo).reduce((acc, item) => {
+		users = Object.keys(clientInfo).reduce(function (acc, item) {
 			if (clientInfo[item].room === userInfo.room) {
 				acc.push(clientInfo[item].username);
 			}
@@ -40,11 +42,11 @@ const sendCurrentConnectedUsers = socket => {
 	return users.join(', ');
 };
 
-io.on('connection', (socket) => {
+io.on('connection', function (socket) {
 	console.log('Connection Established through Socket.IO');
 
 	// A request to join a specific chat room
-	socket.on('joinRoom', user => {
+	socket.on('joinRoom', function (user) {
 		// Join a room - http://socket.io/docs/rooms-and-namespaces/#rooms
 		socket.join(user.room);
 
@@ -52,7 +54,7 @@ io.on('connection', (socket) => {
 		clientInfo[socket.id] = user;
 
 		// Joining Message
-		const joiningMessage = `${clientInfo[socket.id].username} has joined! Say Hi!`		
+		var joiningMessage = clientInfo[socket.id].username + ' has joined! Say Hi!';
 
 		// Notify all other clients belonging to the specified chat room that a new socket has joined
 		// http://socket.io/docs/rooms-and-namespaces/#joining-and-leaving
@@ -61,8 +63,8 @@ io.on('connection', (socket) => {
 
 	// Socket Disconnect
 	// http://socket.io/docs/rooms-and-namespaces/#default-namespace
-	socket.on('disconnect', () => {
-		const userData = clientInfo[socket.id];
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
 		// Confirm that this socket was part of a chatroom
 		// It is entirely possible to use the Chat App without joining a chatroom
 		if (typeof userData !== 'undefined') {
@@ -71,18 +73,17 @@ io.on('connection', (socket) => {
 			socket.leave(userData.room);
 
 			// Leaving Message
-			const leaveMessage = `${clientInfo[socket.id].username} has left`;
+			var leaveMessage = clientInfo[socket.id].username + ' has left';
 
 			io.to(clientInfo[socket.id].room).emit('message', sendSysMessage(leaveMessage));
 
-			delete clientInfo[socket.id];			
+			delete clientInfo[socket.id];
 		}
-
 	});
 
 	socket.emit('message', sendSysMessage('Welcome new traveller!'));
 
-	socket.on('message', data => {
+	socket.on('message', function (data) {
 		// socket.broadcast.emit('message', data); -- Send to all connections EXCEPT emitter
 
 		// http://momentjs.com/docs/#/displaying/unix-timestamp-milliseconds/
@@ -96,9 +97,11 @@ io.on('connection', (socket) => {
 			// io.emit('message', data); // Send to all connections including emitter
 
 			// -- Send to all connections (belonging to the same room) including emitter
-			io.to(clientInfo[socket.id].room).emit('message', data); 
+			io.to(clientInfo[socket.id].room).emit('message', data);
 		}
 	});
 });
 
-http.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+http.listen(PORT, function () {
+	return console.log('Listening on Port ' + PORT);
+});
